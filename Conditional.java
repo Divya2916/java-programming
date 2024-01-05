bc0faa6ce462045a06a07751298e42dfd393dbe1
Divya2916 <121809915+Divya2916@users.noreply.github.com>
2024-01-05 11:22:54 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Conditional
+{
+	public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter number");
+	int x = sc.nextInt();
+	if(x%2==0)
+	{
+	System.out.println("number is even");
+	}
+	else
+	{
+	System.out.println("number is odd");
+	}
+	}
+}
+	
